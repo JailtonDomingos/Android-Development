eddd9c58fb35738b84fb44490bd5255c05b194ca
JailtonDomingos <development.j@outlook.com>
2018-03-20 00:12:35 -0300

    commit

--- MilitaryArms/app/src/main/java/com/projetc/domingos/militaryarms/MainActivity.java
+++ MilitaryArms/app/src/main/java/com/projetc/domingos/militaryarms/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        MainActivity.setContentView(R.layout.activity_main);
     }
     public void onClick(View view){
         switch (view.getId()){
