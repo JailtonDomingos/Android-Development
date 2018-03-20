ebb15d23497a54d9ead0156452f13ede9845e721
JailtonDomingos <development.j@outlook.com>
2018-03-20 00:42:46 -0300

    commit

--- MilitaryArms/app/src/main/java/com/projetc/domingos/militaryarms/MainActivity.java
+++ MilitaryArms/app/src/main/java/com/projetc/domingos/militaryarms/MainActivity.java
@@ -4,16 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MainActivity.setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        Button buttonLogin = findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(this);
+        Button buttonCancel = findViewById(R.id.buttonCancel);
+        buttonCancel.setOnClickListener(this);
+
     }
     public void onClick(View view){
         switch (view.getId()){
@@ -26,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
                 } else {
                     //String texto = R.string.wrong_password;
-                    Toast.makeText(this, R.string.invalid_password, Toast.LENGTH_LONG);
+                    Toast.makeText(this, R.string.invalid_password, Toast.LENGTH_LONG).show();
 
                 }
                 break;
--- MilitaryArms/app/src/main/java/com/projetc/domingos/militaryarms/SecondActivity.java
+++ MilitaryArms/app/src/main/java/com/projetc/domingos/militaryarms/SecondActivity.java
@@ -4,26 +4,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by jailton.domingos on 14/03/2018.
  */
 
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        Button buttonVoltar = findViewById(R.id.buttonVoltar);
+        buttonVoltar.setOnClickListener(this);
+
     }
 
     public void onClick(View view){
         switch (view.getId()){
             case R.id.buttonVoltar:
-                Intent intentVoltar = new Intent(this, MainActivity.class);
-                startActivity(intentVoltar);
+                //Intent intentVoltar = new Intent(SecondActivity.this, MainActivity.class);
+                //startActivity(intentVoltar);
+                finish();
                 break;
         }
     }
