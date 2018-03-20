753b17d2e0f31be596a878e37057ce5fa82f24ee
JailtonDomingos <development.j@outlook.com>
2018-03-19 23:50:42 -0300

    commit

--- MilitaryArms/app/src/main/java/com/projetc/domingos/militaryarms/MainActivity.java
+++ MilitaryArms/app/src/main/java/com/projetc/domingos/militaryarms/MainActivity.java
@@ -4,46 +4,39 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    //final Button buttonLogin    = findViewById(R.id.buttonLogin);
+    //final Button buttonCancel   = findViewById(R.id.buttonCancel);
+    final EditText formLogin    = findViewById(R.id.editTextLogin);
+    final EditText formSenha    = findViewById(R.id.editTextSenha);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final Button buttonLogin    = findViewById(R.id.buttonLogin);
-        final Button buttonCancel   = findViewById(R.id.buttonCancel);
-        final EditText formLogin    = findViewById(R.id.editTextLogin);
-        final EditText formSenha    = findViewById(R.id.editTextSenha);
-
-        final Intent intentLogin = new Intent(MainActivity.this, SecondActivity.class);
-        final Intent intentErro;
-        intentErro = new Intent(MainActivity.this, ThirdActivity.class);
-
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    }
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.buttonLogin:
                 if (formLogin.getText().toString().equals("bacon") && formSenha.getText().toString().equals("123")) {
+                    Intent intentLogin    = new Intent(this, SecondActivity.class);
                     startActivity(intentLogin);
 
                 } else {
-                    startActivity(intentErro);
+                    //String texto = R.string.wrong_password;
+                    Toast.makeText(this, R.string.invalid_password, Toast.LENGTH_LONG);
 
                 }
-            }
-        });
-        buttonCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MainActivity.this.finish();
+                break;
+            case R.id.buttonCancel:
+                 MainActivity.this.finish();
+                break;
 
-            }
-        });
-
-
+        }
     }
 }
--- MilitaryArms/app/src/main/java/com/projetc/domingos/militaryarms/SecondActivity.java
+++ MilitaryArms/app/src/main/java/com/projetc/domingos/militaryarms/SecondActivity.java
@@ -1,9 +1,9 @@
 package com.projetc.domingos.militaryarms;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 /**
  * Created by jailton.domingos on 14/03/2018.
@@ -17,17 +17,15 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Button buttonVoltar = findViewById(R.id.buttonVoltar);
+    }
 
-        buttonVoltar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackPressed();
-
-            }
-        });
-
-
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.buttonVoltar:
+                Intent intentVoltar = new Intent(this, MainActivity.class);
+                startActivity(intentVoltar);
+                break;
+        }
     }
 
 }
